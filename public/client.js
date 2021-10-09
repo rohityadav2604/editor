206393cf78b6ba4abf7b7a0d65e4459377425b4d
Rohit <rohityadav2604@gmail.com>
2021-10-09 18:48:31 +0530

    collab with id done

--- public/client.js
+++ public/client.js
@@ -1,4 +1,5 @@
-var socket = io('http://localhost:3000');
+// var socket = io('http://localhost:3000');
+var socket = io();
 const l = console.log
 function getEl(id) {
     return document.getElementById(id)
--- server.js
+++ server.js
@@ -1,16 +1,56 @@
-
-const http = require('http').createServer()
+const express = require("express");
+const app = express();
+const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const port = 3000
-http.listen(port, () => console.log(`server listening on port: ${port}`))
+let id;
 
+app.use(express.static(__dirname+'/public'));
+app.get("/" , (req , res)=>{res.send("hello")});
+
+// app.get('*' ,(req , res)=>{
+   
+//     res.sendFile(__dirname+'/public/index.html');
+// } )
+
+app.get("/user/:userid" , (req , res)=>{
+      id = req.params.userid
+      console.log(id);
+   // socket.id = req.params.userid;
+   // console.log(socket.id + "hello")
+    res.sendFile( __dirname +'/public/index.html' );
+})
+
+let arr = [];
 io.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('message', (evt) => {
-      //  console.log(evt)
-        socket.broadcast.emit('message', evt)
-    })
+    //console.log('connected')
+    arr.push(socket.id);
+    console.log(socket.id);
+    console.log("----->")
+
+    if(id==0)
+    {
+        socket.on('message', (evt) => {
+            //console.log(evt);
+         //socket.broadcast.to(socket.id).emit('message', evt)
+           io.to(arr[1]).emit('message', evt);
+    
+        })
+    }
+    if(id==1)
+    {
+        socket.on('message', (evt) => {
+            //console.log(evt);
+         //socket.broadcast.to(socket.id).emit('message', evt)
+           io.to(arr[0]).emit('message', evt);
+    
+        })
+
+    }
+    
 })
 io.on('disconnect', (evt) => {
     console.log('some people left');
 })
+
+http.listen(port, () => console.log(`server listening on port: ${port}`))
